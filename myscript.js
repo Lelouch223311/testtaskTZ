d553068d7cca3a2f192ae912c64705577b7a4666
Lelouch223311 <foracademy228@mail.ru>
2025-03-04 18:06:55 +0300

    deploy to github

---
+++ myscript.js
@@ -0,0 +1,12 @@
+// tut pusto bro | Uje net)
+
+function viveskaoff() {
+
+  document.querySelector('.viveska').style.display = 'none';
+  document.querySelector('.fbb70').style.display = 'none';
+  document.querySelector('.closebtn').style.display = 'none';
+
+}
+
+
+/* Тут когда то был код для выпадающеgo списка :c */
